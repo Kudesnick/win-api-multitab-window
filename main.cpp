99d8506be4e917558d4d4e8c4b94150b21f84b73
Stulov Tikhon <kudesnick@inbox.ru>
2022-12-01 11:54:47 +0300

    Небольшой рефакторинг

--- main.cpp
+++ main.cpp
@@ -57,24 +57,30 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+    LRESULT result = 0;
+
     switch (uMsg)
     {
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
+        case WM_DESTROY:
+        {
+            PostQuitMessage(0);
+        }
+        break;
 
-    case WM_PAINT:
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
+        case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
 
-        // All painting occurs here, between BeginPaint and EndPaint.
-        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-        EndPaint(hwnd, &ps);
+            // All painting occurs here, between BeginPaint and EndPaint.
+            FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+            EndPaint(hwnd, &ps);
+        }
+        break;
+
+        default:
+            result = DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
-    return 0;
-    }
 
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    return result;
 }
-
